232b00aaceb1920cc3aeb27562a7038bc0da0ff3
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-04-28 18:35:04 +0700

    Email validate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import './style.css';
 
-let btn = document.querySelector('form');
-btn.addEventListener('submit', (e) => {
-    e.defaultPrevented();
-});
+class ValidateForm {
+    constructor() {
+        this.userEmail = document.getElementById('user_email');
+        this.btn = document.querySelector('form');
+
+        this.init();
+    }
+    init() {
+        this.btn.addEventListener('submit', (e) => {
+            e.preventDefault();
+        });
+        this.userEmail.addEventListener('input', () => {
+            this.HandleValidateEmail();
+        });
+        this.userEmail.addEventListener('change', () => {
+            this.HandleValidateEmail();
+        });
+    }
+
+    HandleValidateEmail() {
+        const emailError = document.getElementById('emailError');
+        if (this.userEmail.validity.valid) {
+            emailError.textContent = '';
+            return;
+        }
+
+        if (this.userEmail.validity.typeMismatch) {
+            emailError.textContent = 'Please enter email address';
+        } else if (this.userEmail.validity.valueMissing) {
+            emailError.textContent =
+                "Because this information is crucial, please don't leave it";
+        } else if (this.userEmail.validity.tooShort) {
+            emailError.textContent = `You need to type ${this.userEmail.maxLength} for valid email, You are ${this.userEmail.value.length} character`;
+        }
+    }
+}
+
+new ValidateForm();
